eee1e0047713fdee6fa1a4a60d4348613d10591b
Allbitsareoff <ken.bourassa@allbitsareoff.com>
2016-02-28 22:00:29 -0500

    Added files via upload

---
+++ ab.MemUtils.pas
@@ -0,0 +1,82 @@
+{* Copyright (C) 2016
+    Ken Bourassa @ http://www.allbitsareoff.com - All Rights Reserved
+ *}
+ unit ab.MemUtils;
+
+interface
+
+type
+  TRedirectHandle = NativeInt;
+
+function RedirectCall(OriginalCode, NewCode: Pointer) : TRedirectHandle;
+procedure RestoreCall(AHandle: TRedirectHandle);
+
+implementation
+
+uses
+  System.SysUtils,
+  WinApi.Windows
+
+  ;
+
+type
+  TJumpData = packed record
+    Jump: Byte;
+    Offset: NativeInt;
+  end;
+
+  TRedirHandleData = packed record
+    OriginalCodeAddr : Pointer;
+    OriginalInstr: TJumpData;
+  end;
+
+
+function RedirectCall(OriginalCode, NewCode: Pointer) : TRedirectHandle;
+var
+  cbBuffer: NativeUint;
+  hRedir: ^TRedirHandleData absolute Result;
+  vJumpData : TJumpData;
+begin
+  Assert(OriginalCode <> nil);
+  Assert(NewCode <> nil);
+  //It would probably be better to prevent redirection for pointers that are in different modules.
+  //Otherwise, this can lead to some really messy results when dynamically loading/unloading DLLs
+  New(hRedir); //New(Result)
+  try
+    if ReadProcessMemory(GetCurrentProcess, OriginalCode, @hRedir.OriginalInstr, SizeOf(hRedir.OriginalInstr), cbBuffer) then
+    begin
+      hRedir.OriginalCodeAddr := OriginalCode;
+      vJumpData.Jump := $E9;
+      vJumpData.Offset := PAnsiChar(NewCode) - PAnsiChar(OriginalCode) - SizeOf(hRedir.OriginalInstr);
+      if not WriteProcessMemory(GetCurrentProcess, OriginalCode, @vJumpData, SizeOf(vJumpData), cbBuffer) then
+      begin
+        Dispose(hRedir);
+        hRedir := nil;
+      end;
+    end else
+      hRedir := nil; //Result := nil;
+  except
+    Dispose(hRedir);
+    raise;
+  end;
+end;
+
+procedure RestoreCall(AHandle: TRedirectHandle);
+var
+  cbBuffer: NativeUint;
+  hRedir: ^TRedirHandleData absolute AHandle;
+begin
+  if hRedir = nil then EXIT;
+
+  try
+    if (hRedir.OriginalCodeAddr <> nil) then
+    begin
+      //If the write fails, I'm not sure how we can recover.
+      WriteProcessMemory(GetCurrentProcess, hRedir.OriginalCodeAddr, @hRedir.OriginalInstr, SizeOf(hRedir.OriginalInstr), cbBuffer);
+    end;
+  finally
+    Dispose(hRedir);
+  end;
+end;
+
+end.
---
+++ ab.Patch.System.SysUtils.pas
@@ -0,0 +1,90 @@
+{* Copyright (C) 2016
+    Ken Bourassa @ http://www.allbitsareoff.com - All Rights Reserved
+ *}
+unit ab.Patch.System.SysUtils;
+
+interface
+
+implementation
+
+uses
+  System.SysUtils,
+  System.StrUtils,
+  ab.MemUtils,
+  ;
+
+var
+  uStringReplaceRedirect : TRedirectHandle;
+
+function abStringReplace(const S, OldPattern, NewPattern: string;
+  Flags: TReplaceFlags): string;
+const
+  FirstIndex = Low(string);
+var
+  SearchStr, Patt, NewStr : string;
+  Offset, I, L, idxStart: Integer;
+begin
+  if rfIgnoreCase in Flags then
+  begin
+    SearchStr := AnsiUpperCase(S);
+    Patt := AnsiUpperCase(OldPattern);
+  end else
+  begin
+    SearchStr := S;
+    Patt := OldPattern;
+  end;
+  NewStr := S;
+  Result := '';
+  if SearchStr.Length <> S.Length then
+  begin
+    I := FirstIndex;
+    L := OldPattern.Length;
+    while I <= High(S) do
+    begin
+      if string.Compare(S, I - FirstIndex, OldPattern, 0, L, True) = 0 then
+      begin
+        Result := Result + NewPattern;
+        Inc(I, L);
+        if not (rfReplaceAll in Flags) then
+        begin
+          Result := Result + S.Substring(I - FirstIndex, MaxInt);
+          Break;
+        end;
+      end
+      else
+      begin
+        Result := Result + S[I];
+        Inc(I);
+      end;
+    end;
+  end
+  else
+  begin
+    Offset := PosEx(Patt, SearchStr);
+    if Offset = 0 then
+      Result := S
+    else
+    begin
+      if not (rfReplaceAll in Flags) then
+        Result := Copy(S, 1, OffSet - 1) + NewPattern + Copy(S, Offset + Length(OldPattern), MaxInt)
+      else
+      begin
+        idxStart := 1;
+        repeat
+          Result := Result + Copy(S, idxStart, OffSet - idxStart) + NewPattern;
+          idxStart := Offset + Length(Patt);
+          Offset := PosEx(Patt, SearchStr, idxStart);
+        until Offset = 0;
+        Result := Result + Copy(S, idxStart, MaxInt);
+      end;
+    end;
+  end;
+end;
+
+initialization
+  uStringReplaceRedirect := RedirectCall(@StringReplace, @abStringReplace)
+
+finalization
+  RestoreCall(uStringReplaceRedirect);
+
+end.
